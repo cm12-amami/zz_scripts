7cef6a3dbef1d39c38a77e4f58a5a26110600230
MSe1969 <mse1969@posteo.de>
2020-11-12 14:12:18 +0100

    Properly handle hardware/qcom and freetype, add local_manifests

--- checkout-12.sh
+++ checkout-12.sh
@@ -19,7 +19,6 @@ external/aac
 external/bluetooth/bluedroid
 external/chromium-libpac
 external/chromium_org/v8
-external/freetype
 external/giflib
 external/libcxx
 external/libexif
@@ -51,6 +50,9 @@ frameworks/opt/net/wifi
 frameworks/opt/telephony
 hardware/libhardware
 hardware/ril
+hardware/qcom/audio/default
+hardware/qcom/display
+hardware/qcom/media/default
 kernel/sony/msm8974
 libcore
 packages/apps/Bluetooth
@@ -77,14 +79,38 @@ system/core
 system/extras/su
 system/media
 vendor/cm
+.repo/local_manifests
 EOF
 }
+
+list_qcom() {
+cat <<EOF
+hardware/qcom/audio-caf/msm8974
+hardware/qcom/display-caf/msm8974
+hardware/qcom/media-caf/msm8974
+EOF
+}
+
 
 TOPDIR=$PWD
 list_repos | while read REPO; do
     cd $REPO
     echo "$PWD"
     git checkout cm-12.1
+    git pull
+    cd $TOPDIR
+done
+
+cd external/freetype
+echo "Exception: freetype"
+git checkout cm-13.0
+git pull
+cd $TOPDIR
+
+list_qcom | while read REPO; do
+    cd $REPO
+    echo "$PWD"
+    git checkout cm-12.1-caf-8974
     git pull
     cd $TOPDIR
 done
